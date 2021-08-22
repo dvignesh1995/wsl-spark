a0373822ef70cbf5b453268176204efebe022101
vignesh d <dvignesh1995@gmail.com>
2021-08-22 18:02:27 +0530

    env var bug fix

--- spark_setup.sh
+++ spark_setup.sh
@@ -11,7 +11,7 @@ wget https://mirrors.estointernet.in/apache/spark/spark-3.1.2/spark-3.1.2-bin-ha
 tar -xvf spark-3.1.2-bin-hadoop3.2.tgz 
 sudo mv spark-3.1.2-bin-hadoop3.2 /opt/spark
 echo "export SPARK_HOME=/opt/spark" >> ~/.profile
-echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.profile
+echo "export PATH=$PATH:/opt/spark/bin:/opt/spark/sbin" >> ~/.profile
 echo "export PYSPARK_PYTHON=/home/user4/usr/bin/python" >> ~/.profile
 sed -i '/^export PATH=/ s/$/"/'  ~/.profile
 sed -i '/^export PATH=/ s/=/="/' ~/.profile
