f9b70ae83b879bd7ef50e0e5d9c9fded86062921
VIGNESH D <35656793+dvignesh1995@users.noreply.github.com>
2022-01-15 16:13:54 +0530

    bug fixed user specific hard code

--- spark_setup.sh
+++ spark_setup.sh
@@ -12,7 +12,7 @@ tar -xvf spark-3.1.2-bin-hadoop3.2.tgz
 sudo mv spark-3.1.2-bin-hadoop3.2 /opt/spark
 echo "export SPARK_HOME=/opt/spark" >> ~/.profile
 echo "export PATH=$PATH:/opt/spark/bin:/opt/spark/sbin" >> ~/.profile
-echo "export PYSPARK_PYTHON=/home/user4/usr/bin/python" >> ~/.profile
+echo "export PYSPARK_PYTHON=~/usr/bin/python" >> ~/.profile
 sed -i '/^export PATH=/ s/$/"/'  ~/.profile
 sed -i '/^export PATH=/ s/=/="/' ~/.profile
 source ~/.profile
